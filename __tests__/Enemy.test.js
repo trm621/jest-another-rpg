5679872739b66e857553269d99f7a513d0f4b701
trm621 <t.maher621@gmail.com>
2021-11-30 19:09:19 -0500

    finished game

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -6,24 +6,23 @@ jest.mock('../lib/Player');
 jest.mock('../lib/Potion');
 
 test('checks to see if an enemy object is made', () => {
-    const enemy = new Enemy('goblin', 'sword');
+    const enemy = new Enemy();
 
-    expect(enemy.name).toBe('goblin');
-    expect(enemy.weapon).toBe('sword');
+    expect(enemy.name).toEqual(expect.any(String));
+    expect(enemy.weapon).toEqual(expect.any(String));
     expect(enemy.health).toEqual(expect.any(Number));
     expect(enemy.strength).toEqual(expect.any(Number));
     expect(enemy.agility).toEqual(expect.any(Number));
-    expect(enemy.potion).toEqual(expect.any(Object));
 });
 
 test("gets enemy's health value", () => {
-    const enemy = new Enemy('goblin', 'sword');
+    const enemy = new Enemy(expect.any(String));
 
     expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
 });
 
 test("checks if enemy is alive or not", () => {
-    const enemy = new Enemy('goblin', 'sword');
+    const enemy = new Enemy();
 
     expect(enemy.isAlive()).toBeTruthy();
 
@@ -33,7 +32,7 @@ test("checks if enemy is alive or not", () => {
 });
 
 test("subtracts from enemy's health", () => {
-    const enemy = new Enemy('goblin', 'sword');
+    const enemy = new Enemy();
     const oldHealth = enemy.health;
 
     enemy.reduceHealth(5);
@@ -46,7 +45,7 @@ test("subtracts from enemy's health", () => {
 });
 
 test("gets enemy's attack value", () => {
-    const enemy = new Enemy('goblin', 'sword');
+    const enemy = new Enemy();
     enemy.strength = 10;
 
     expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
@@ -54,7 +53,7 @@ test("gets enemy's attack value", () => {
 });
 
 test ("gets a description of the enemy", () => {
-    const enemy = new Enemy('goblin', 'sword');
+    const enemy = new Enemy;
 
     expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
     expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
